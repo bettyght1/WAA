dc57d577ce23f4b3a9966c51efef6f14a527e684
bettyght1 <bdesta@miu.edu>
2021-09-03 11:18:50 -0500

    Lab2_Valid

---
+++ Lab2_Valid/src/main/java/com/students/domain/Phone.java
@@ -0,0 +1,55 @@
+/**
+ * 
+ */
+package com.students.domain;
+
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+
+/**
+ * @author B.Pirasanth
+ *
+ */
+public class Phone implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Digits(integer = 3, fraction =0, message = "{Size.area.validation}")
+	@Size(min =100, max= 999)
+ 	private Integer area;
+	@Digits(integer = 3, fraction =0, message ="{Size.prefix.validation}")
+	@Size(min = 100, max = 999)
+ 	private Integer prefix;
+	@Digits(integer = 4, fraction =0, message = "{Size.number.validation}")
+	@Size(min =1000, max= 9999)
+ 	private Integer number;
+	
+
+ 
+	public Integer getArea() {
+		return area;
+	}
+
+	public void setArea(Integer area) {
+		this.area = area;
+	}
+
+ 	public Integer getNumber() {
+		return number;
+	}
+
+	public void setNumber(Integer number) {
+		this.number = number;
+	}
+
+	public Integer getPrefix() {
+		return prefix;
+	}
+
+	public void setPrefix(Integer prefix) {
+		this.prefix = prefix;
+	}
+}
